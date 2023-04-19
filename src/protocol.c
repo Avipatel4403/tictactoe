3f5b1148ffddbf440a8668825b6a1334aa1aa146
Avipatel4403 <avipatel4403@gmail.com>
2023-04-19 17:18:56 -0400

    Basics

--- src/protocol.c
+++ src/protocol.c
@@ -1,19 +1,11 @@
-#include <assert.h>
-#include <fcntl.h>
-#include <glob.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <unistd.h>
+
 
 /*
 format the message being sent from server to client and vice versa 
 into something that is readable for the server
 Ex:
 Play Avi ---> PLAY|3|Avi
+
+if you dont want this i can 
 */
 
--- src/ttts.c
+++ src/ttts.c
@@ -1,5 +1,9 @@
+// Compile:  gcc -pthread -Wall -Wextra ttts.c -o ttts
+
 #define _POSIX_C_SOURCE 200809L
+#include <errno.h>
 #include <netdb.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,49 +12,259 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#define _POSIX_C_SOURCE 200809L
+#include <unistd.h>
 
-#define EMPTY 0
-#define X 1
-#define O 2
-#define DRAW 3
+#define QUEUE_SIZE 8
 
-//this is needed to handle signals
+//Defintions
+#define EMPTY 0 //Empty Space on the board
+#define X 1 //Game Status: X is the winner
+#define O 2 //Games Status: O is the winner
+#define DRAW 3 //Self Explanatory
+
+//Global
+
+
+typedef struct Client{
+    int SOCK; //Players Connection
+    int STATUS; //In a game or not 
+    char PIECE; // NULL X or O
+    char* NAME; //players name
+    struct Client* next; //Optional for now
+}Client;
+
+typedef struct Game{
+    Client* one;
+    Client* two;
+    char board[3][3];
+    int status;
+    struct Game* next;
+
+}Game;
+
+int numOfGames;
+int numOfClients;
+Client* Clients;
+Game* Games;
+
+void initializeServer() {
+    numOfClients = 0;
+    numOfClients = 0;
+    Clients = NULL;
+    Games = NULL;
+}
+
 volatile int active = 1;
 
-void handle(int signum) {
+void handler(int signum) {
     active = 0;
 }
 
+// set up signal handlers for primary thread
+// return a mask blocking those signals for worker threads
+// FIXME should check whether any of these actually succeeded
+void install_handlers(sigset_t* mask) {
+    struct sigaction act;
+    act.sa_handler = handler;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGTERM, &act, NULL);
+
+    sigemptyset(mask);
+    sigaddset(mask, SIGINT);
+    sigaddset(mask, SIGTERM);
+}
+
+// data to be sent to worker threads
+struct connection_data {
+    struct sockaddr_storage addr;
+    socklen_t addr_len;
+    int fd;
+};
+
+int open_listener(char* service, int queue_size) {
+    struct addrinfo hint, *info_list, *info;
+    int error, sock;
+
+    // initialize hints
+    memset(&hint, 0, sizeof(struct addrinfo));
+    hint.ai_family = AF_UNSPEC;
+    hint.ai_socktype = SOCK_STREAM;
+    hint.ai_flags = AI_PASSIVE;
+
+    // obtain information for listening socket
+    error = getaddrinfo(NULL, service, &hint, &info_list);
+    if (error) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(error));
+        return -1;
+    }
+
+    // attempt to create socket
+    for (info = info_list; info != NULL; info = info->ai_next) {
+        sock = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+
+        // if we could not create the socket, try the next method
+        if (sock == -1) continue;
+
+        // bind socket to requested port
+        error = bind(sock, info->ai_addr, info->ai_addrlen);
+        if (error) {
+            close(sock);
+            continue;
+        }
+
+        // enable listening for incoming connection requests
+        error = listen(sock, queue_size);
+        if (error) {
+            close(sock);
+            continue;
+        }
+
+        // if we got this far, we have opened the socket
+        break;
+    }
+
+    freeaddrinfo(info_list);
+
+    // info will be NULL if no method succeeded
+    if (info == NULL) {
+        fprintf(stderr, "Could not bind\n");
+        return -1;
+    }
+
+    return sock;
+}
+
+#define BUFSIZE 256
+#define HOSTSIZE 100
+#define PORTSIZE 10
+void* read_data(void* arg) {
+    struct connection_data* con = arg;
+    char buf[BUFSIZE + 1], host[HOSTSIZE], port[PORTSIZE];
+    int bytes, error;
+
+    error = getnameinfo(
+        (struct sockaddr*)&con->addr, con->addr_len,
+        host, HOSTSIZE,
+        port, PORTSIZE,
+        NI_NUMERICSERV);
+    if (error) {
+        fprintf(stderr, "getnameinfo: %s\n", gai_strerror(error));
+        strcpy(host, "??");
+        strcpy(port, "??");
+    }
+
+    printf("Connection from %s:%s\n", host, port);
+
+    while (active && (bytes = read(con->fd, buf, BUFSIZE)) > 0) {
+        buf[bytes] = '\0';
+        printf("[%s:%s] read %d bytes |%s|\n", host, port, bytes, buf);
+    }
+
+    if (bytes == 0) {
+        printf("[%s:%s] got EOF\n", host, port);
+    } else if (bytes == -1) {
+        printf("[%s:%s] terminating: %s\n", host, port, strerror(errno));
+    } else {
+        printf("[%s:%s] terminating\n", host, port);
+    }
+
+    close(con->fd);
+    free(con);
+
+    return NULL;
+}
+
 //check board
-
-
 char* protocol(char* message);
-
-
-
 int checkBoard(char** board);
 int checkMove();
 
 int main(int argc, char **argv) {
+    
+    initializeServer();
 
-    //signals 
-    struct sigaction act;
-    struct sigaction old;
-   
-   
-    //need to create a single socket for server
-    if(argc < 2){
-        perror("missing port or socket");
+    sigset_t mask;
+    struct connection_data* con;
+    int error;
+    pthread_t tid;
+
+    char* service = argc == 2 ? argv[1] : "15000";
+
+    install_handlers(&mask);
+
+    int listener = open_listener(service, QUEUE_SIZE);
+    if (listener < 0) exit(EXIT_FAILURE);
+
+    printf("Listening for incoming connections on %s\n", service);
+
+    while (active) {
+        con = (struct connection_data*)malloc(sizeof(struct connection_data));
+        con->addr_len = sizeof(struct sockaddr_storage);
+
+        con->fd = accept(listener,
+                         (struct sockaddr*)&con->addr,
+                         &con->addr_len);
+
+        if (con->fd < 0) {
+            perror("accept");
+            free(con);
+            // TODO check for specific error conditions
+            continue;
+        }
+
+        // temporarily disable signals
+        // (the worker thread will inherit this mask, ensuring that SIGINT is
+        // only delivered to this thread)
+        error = pthread_sigmask(SIG_BLOCK, &mask, NULL);
+        if (error != 0) {
+            fprintf(stderr, "sigmask: %s\n", strerror(error));
+            exit(EXIT_FAILURE);
+        }
+
+        error = pthread_create(&tid, NULL, read_data, con);
+        if (error != 0) {
+            fprintf(stderr, "pthread_create: %s\n", strerror(error));
+            close(con->fd);
+            free(con);
+            continue;
+        }
+
+        // automatically clean up child threads once they terminate
+        pthread_detach(tid);
+
+        // unblock handled signals
+        error = pthread_sigmask(SIG_UNBLOCK, &mask, NULL);
+        if (error != 0) {
+            fprintf(stderr, "sigmask: %s\n", strerror(error));
+            exit(EXIT_FAILURE);
+        }
     }
 
-    //server socket
-    struct sockaddr_storage server;
+    puts("Shutting down");
+    close(listener);
 
-    socklen_t servlen = sizeof(server);
+    // returning from main() (or calling exit()) immediately terminates all
+    // remaining threads
+
+    // to allow threads to run to completion, we can terminate the primary thread
+    // without calling exit() or returning from main:
+    //   pthread_exit(NULL);
+    // child threads will terminate once they check the value of active, but
+    // there is a risk that read() will block indefinitely, preventing the
+    // thread (and process) from terminating
+
+    // to get a timely shut-down of all threads, including those blocked by
+    // read(), we will could maintain a global list of all active thread IDs
+    // and use pthread_cancel() or pthread_kill() to wake each one
 
     return EXIT_SUCCESS;
 }
-int checkMove(char** board,int row, int column){
+
+int isValidMove(char** board,int row, int column){
     if(board[row][column] != EMPTY){
         return 1;
     }
